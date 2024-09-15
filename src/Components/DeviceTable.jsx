c0b76c452a28a2c9c7064a6053811b01a17f343b
Wendy Medina <endyleyms@gmail.com>
2024-09-14 20:43:19 -0500

    format document

--- src/Components/DeviceTable.jsx
+++ src/Components/DeviceTable.jsx
@@ -1,196 +1,204 @@
-import React, { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import { NavLink } from 'react-router-dom';
-import {deleteEquipo, getAllEquipos, updateEquipo} from '../Servicios/equipos';
-import Swal from 'sweetalert2';
+import React, { useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { NavLink } from 'react-router-dom'
+import { deleteEquipo, getAllEquipos, updateEquipo } from '../Servicios/equipos'
+import Swal from 'sweetalert2'
 
-function DeviceTable() {
-    const [editingDeviceId, setEditingDeviceId] = useState(null);
-    const [searchTerm, setSearchTerm] = useState('');
-    const [devices, setDevices] = useState();
-    const [isLoading, setIsLoading] = useState(true);
-    const [editarDisco, setEditarDisco] = useState('');
-    const [editarMemoria, setEditarMemoria] = useState('');
-    const [isEquipoUpdated, setIsEquipoUpdated] = useState(false); 
-    const [deleteDevice, setDeleteDevice] = useState ('');
+function DeviceTable () {
+  const [editingDeviceId, setEditingDeviceId] = useState(null)
+  const [searchTerm, setSearchTerm] = useState('')
+  const [devices, setDevices] = useState()
+  const [isLoading, setIsLoading] = useState(true)
+  const [editarDisco, setEditarDisco] = useState('')
+  const [editarMemoria, setEditarMemoria] = useState('')
+  const [isEquipoUpdated, setIsEquipoUpdated] = useState(false)
+  const [deleteDevice, setDeleteDevice] = useState('')
 
-    const { t } = useTranslation();
+  const { t } = useTranslation()
 
-    //funcion para traer todos los equipos
-    const allEquipos = async () => {
-        try {
-            const data = await getAllEquipos();
-            setDevices(data);
-        } catch (error) {
-            console.error("Error trayendo los equipos" + error)
-        } finally {
-            setIsLoading(false);
-        }
-    };
-
-    const handleEditClick = (id) => {
-        setEditingDeviceId(id);
-    };
-
-    const handleSaveClick = (id) => {
-        try {
-            const editar = updateEquipo(id, {
-                memoria_ram : editarMemoria,
-                disco_duro: editarDisco
-            })
-            if(editar){
-            setIsEquipoUpdated(true);
-            setEditingDeviceId(null);
-            Swal.fire({
-                title: 'Equipo editado',
-                text: 'El equipo ha sido editado exitosamente',
-                icon: 'success',
-                confirmButtonText: 'Ok',
-                });
-            }
-            
-        } catch (error) {
-            console.error('Error updating user:', error);
-            Swal.fire({
-                title: 'Error',
-                text: 'Hubo un problema al editar el equipo',
-                icon: 'error',
-                confirmButtonText: 'Ok',
-            }); 
-        }
-        setEditingDeviceId(null);
-    };
-
-    const handleDeleteEquipo = async (id) => {
-        try {
-            const eliminarEquipo = await deleteEquipo(id);
-            if(eliminarEquipo){  
-                setIsEquipoUpdated(true);              
-                Swal.fire({
-                    title:'Equipo eliminado',
-                    text:'El equipo ha sido eliminado correctamente',
-                    icon:'success',
-                    confirmButtonText:'Ok',
-                });
-            }
-        } catch (error) {
-            console.error('Error eliminando equipo'+ error);
-            Swal.fire({
-                title:'Error',
-                text:'Error al intentar eliminar el equipo',
-                icon:'error',
-                confirmButtonText: 'Ok',
-            })
-        }
+  //funcion para traer todos los equipos
+  const allEquipos = async () => {
+    try {
+      const data = await getAllEquipos()
+      setDevices(data)
+    } catch (error) {
+      console.error('Error trayendo los equipos' + error)
+    } finally {
+      setIsLoading(false)
     }
+  }
 
-    useEffect(() => {
-        allEquipos();
-        if(isEquipoUpdated){
-            allEquipos();
-            setIsEquipoUpdated(false);
-        }
-    }, [isEquipoUpdated])
-    /*const filteredDevices = devices.filter((device) =>
+  const handleEditClick = id => {
+    setEditingDeviceId(id)
+  }
+
+  const handleSaveClick = id => {
+    try {
+      const editar = updateEquipo(id, {
+        memoria_ram: editarMemoria,
+        disco_duro: editarDisco
+      })
+      if (editar) {
+        setIsEquipoUpdated(true)
+        setEditingDeviceId(null)
+        Swal.fire({
+          title: 'Equipo editado',
+          text: 'El equipo ha sido editado exitosamente',
+          icon: 'success',
+          confirmButtonText: 'Ok'
+        })
+      }
+    } catch (error) {
+      console.error('Error updating user:', error)
+      Swal.fire({
+        title: 'Error',
+        text: 'Hubo un problema al editar el equipo',
+        icon: 'error',
+        confirmButtonText: 'Ok'
+      })
+    }
+    setEditingDeviceId(null)
+  }
+
+  const handleDeleteEquipo = async id => {
+    try {
+      const eliminarEquipo = await deleteEquipo(id)
+      if (eliminarEquipo) {
+        setIsEquipoUpdated(true)
+        Swal.fire({
+          title: 'Equipo eliminado',
+          text: 'El equipo ha sido eliminado correctamente',
+          icon: 'success',
+          confirmButtonText: 'Ok'
+        })
+      }
+    } catch (error) {
+      console.error('Error eliminando equipo' + error)
+      Swal.fire({
+        title: 'Error',
+        text: 'Error al intentar eliminar el equipo',
+        icon: 'error',
+        confirmButtonText: 'Ok'
+      })
+    }
+  }
+
+  useEffect(() => {
+    allEquipos()
+    if (isEquipoUpdated) {
+      allEquipos()
+      setIsEquipoUpdated(false)
+    }
+  }, [isEquipoUpdated])
+  /*const filteredDevices = devices.filter((device) =>
         Object.values(device).some((value) =>
             typeof value === 'string' && value.toLowerCase().includes(searchTerm.toLowerCase())
         )
     );*/
 
-    return (
-        <>
-        <div className="mb-4 flex w-full flex-col items-center justify-center">
-            <input
-                type="text"
-                placeholder={t('device.search')}
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                className="input input-bordered w-full max-w-xs"
-            />
-        </div>
-        <div className="overflow-x-auto">
-            <table className="table rounded-lg bg-Blue50 p-6 shadow-lg">
-                <thead>
-                    <tr>
-                        <th>{t('device.id')}</th>
-                        <th>{t('device.brand')}</th>
-                        <th>{t('device.serial')}</th>
-                        <th>{t('device.type')}</th>
-                        <th>{t('device.ram')}</th>
-                        <th>{t('device.disk')}</th>
-                        <th>{t('device.processor')}</th>
-                        <th>{t('device.date')}</th>
-                        <th>{t('device.user')}</th>
-                        <th>{t('device.actions')}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        devices?.map((device) => (
-                            <tr key={device.id_equipo}>
-                                <td>{device.id_equipo}</td>
-                                <td>{device.marca.marca_fabricante}</td>
-                                <td>{device.serial}</td>
-                                <td>{device.TipoDispositivo.descripcion}</td>
-                                <td>
-                                    {editingDeviceId === device.id_equipo ?(
-                                        <input
-                                        type="text"
-                                        name="role"
-                                        value={editarMemoria}
-                                        onChange={(e)=> setEditarMemoria(e.target.value)}
-                                        className="input input-sm input-bordered w-full max-w-xs"
-                                        placeholder={device.memoria_ram}
-                                        />
-                                    ) : (
-                                        <span className="badge badge-ghost badge-sm"> {device.memoria_ram}</span>
-                                    )}
-                                    </td>
-                                <td>
-                                    {editingDeviceId === device.id_equipo ?(
-                                        <input
-                                        type="text"
-                                        name="role"
-                                        value={editarDisco}
-                                        onChange={(e)=> setEditarDisco(e.target.value)}
-                                        className="input input-sm input-bordered w-full max-w-xs"
-                                        placeholder={device.disco_duro}
-                                        />
-                                    ) : (
-                                        <span className="badge badge-ghost badge-sm"> {device.disco_duro}</span>
-                                    )}
-                                </td>
-                                <td>{device.procesador}</td>
-                                <td>{device.fecha_registro}</td>
-                                <td>{device.UsuarioResponsable.nombres}</td>
-                                <td>
-                                    {editingDeviceId === device.id_equipo ? (
-                                        <button
-                                            className="btn btn-xs bg-lime-700 text-white hover:bg-lime-600"
-                                            onClick={() => handleSaveClick(device.id_equipo)}
-                                        >Guardar</button>
-                                    ) : (
-                                        <div className='flex gap-2'>
-                                            <button
-                                                className="btn btn-outline btn-xs bg-Blue400 text-white hover:bg-Blue600"
-                                                onClick={() => handleEditClick(device.id_equipo)}
-                                            >Editar</button>
-                                            <button
-                                                className="btn btn-outline btn-xs bg-Blue400 text-white hover:bg-Blue600"
-                                                onClick={() => handleDeleteEquipo(device.id_equipo)}
-                                            >Eliminar</button>
-                                        </div>
-                                    )}
-                                </td>
-                            </tr>
-                        ))
-                    }
-
-                </tbody>
-            </table>
-        </div>
-        </>
-    );
+  return (
+    <>
+      <div className='mb-4 flex w-full flex-col items-center justify-center'>
+        <input
+          type='text'
+          placeholder={t('device.search')}
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+          className='input input-bordered w-full max-w-xs'
+        />
+      </div>
+      <div className='overflow-x-auto'>
+        <table className='table rounded-lg bg-Blue50 p-6 shadow-lg'>
+          <thead>
+            <tr>
+              <th>{t('device.id')}</th>
+              <th>{t('device.brand')}</th>
+              <th>{t('device.serial')}</th>
+              <th>{t('device.type')}</th>
+              <th>{t('device.ram')}</th>
+              <th>{t('device.disk')}</th>
+              <th>{t('device.processor')}</th>
+              <th>{t('device.date')}</th>
+              <th>{t('device.user')}</th>
+              <th>{t('device.actions')}</th>
+            </tr>
+          </thead>
+          <tbody>
+            {devices?.map(device => (
+              <tr key={device.id_equipo}>
+                <td>{device.id_equipo}</td>
+                <td>{device.marca.marca_fabricante}</td>
+                <td>{device.serial}</td>
+                <td>{device.TipoDispositivo.descripcion}</td>
+                <td>
+                  {editingDeviceId === device.id_equipo ? (
+                    <input
+                      type='text'
+                      name='role'
+                      value={editarMemoria}
+                      onChange={e => setEditarMemoria(e.target.value)}
+                      className='input input-sm input-bordered w-full max-w-xs'
+                      placeholder={device.memoria_ram}
+                    />
+                  ) : (
+                    <span className='badge badge-ghost badge-sm'>
+                      {' '}
+                      {device.memoria_ram}
+                    </span>
+                  )}
+                </td>
+                <td>
+                  {editingDeviceId === device.id_equipo ? (
+                    <input
+                      type='text'
+                      name='role'
+                      value={editarDisco}
+                      onChange={e => setEditarDisco(e.target.value)}
+                      className='input input-sm input-bordered w-full max-w-xs'
+                      placeholder={device.disco_duro}
+                    />
+                  ) : (
+                    <span className='badge badge-ghost badge-sm'>
+                      {' '}
+                      {device.disco_duro}
+                    </span>
+                  )}
+                </td>
+                <td>{device.procesador}</td>
+                <td>{device.fecha_registro}</td>
+                <td>{device.UsuarioResponsable.nombres}</td>
+                <td>
+                  {editingDeviceId === device.id_equipo ? (
+                    <button
+                      className='btn btn-xs bg-lime-700 text-white hover:bg-lime-600'
+                      onClick={() => handleSaveClick(device.id_equipo)}
+                    >
+                      Guardar
+                    </button>
+                  ) : (
+                    <div className='flex gap-2'>
+                      <button
+                        className='btn btn-outline btn-xs bg-Blue400 text-white hover:bg-Blue600'
+                        onClick={() => handleEditClick(device.id_equipo)}
+                      >
+                        Editar
+                      </button>
+                      <button
+                        className='btn btn-outline btn-xs bg-Blue400 text-white hover:bg-Blue600'
+                        onClick={() => handleDeleteEquipo(device.id_equipo)}
+                      >
+                        Eliminar
+                      </button>
+                    </div>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  )
 }
 
-export default DeviceTable;
+export default DeviceTable
--- src/Pages/Device.jsx
+++ src/Pages/Device.jsx
@@ -1,107 +1,105 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
-import { createEquipos } from '../Servicios/equipos';
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { NavLink } from 'react-router-dom'
+import { createEquipos } from '../Servicios/equipos'
 
-function Device() {
-    const { t } = useTranslation();
-    const crearEquipo = async (e) => {
-        e.preventDefault();
-        try {
-            const fecha_registro = new Date();
-            const sendData = await createEquipos({
-                id_marca
-            })
-        } catch (error) {
-            
-        }
-        createEquipos();
-    }
-    return (
-        <div className='flex min-h-screen items-center justify-center p-4'>
-            <div className='h-[600px] w-full max-w-md rounded-lg bg-Blue100 p-6 shadow-lg'>
-                <h2 className='mb-6 text-center text-2xl font-bold text-gray-800'>{t('form.tittleform')}</h2>
-                <form className='space-y-4'>
+// Para la funcion de crear
+// fecha_registro: enviar como new Date
+// id_usuario: get de usuarios -> Select
+// id_marca : get de las marcas -> SELECT
+// serial : input
+// id_tipo : tipo de dispositivo, get de dispositivos -> Select
+// memoria_ram : input
+// disco_duro : input
+// procesador: input
 
-                    <label className='input input-bordered flex items-center gap-2'>
-                        <input
-                            type="text"
-                            className='flex-1 border-none outline-none'
-                            placeholder={t('form.user')}
-                            required="true"
-                        />
-                    </label>
+function Device () {
+  const { t } = useTranslation()
 
-                    <label className='input input-bordered flex items-center gap-2'>
-                        <input
-                            type="text"
-                            className='flex-1 border-none outline-none'
-                            placeholder={t('form.brand')}
-                            required="true"
-                        />
-                    </label>
-
-                    <label className='input input-bordered flex items-center gap-2'>
-                        <input
-                            type="text"
-                            className='flex-1 border-none outline-none'
-                            placeholder={t('form.serial')}
-                            required="true"
-                        />
-                    </label>
-
-                    <label className='input input-bordered flex items-center gap-2'>
-                        <select
-                            className='flex-1 border-none outline-none bg-white'
-                            placeholder={t('form.equipmentType')}
-                        >
-                            <option value="">{t('form.selectType')}</option>
-                            <option value="desktop">{t('form.desktop')}</option>
-                            <option value="laptop">{t('form.laptop')}</option>
-                        </select>
-                    </label>
-
-                    <label className='input input-bordered flex items-center gap-2'>
-                        <input
-                            type="text"
-                            className='flex-1 border-none outline-none'
-                            placeholder={t('form.ramCapacity')}
-                            required="true"
-                        />
-                    </label>
-
-                    <label className='input input-bordered flex items-center gap-2'>
-                        <input
-                            type="text"
-                            className='flex-1 border-none outline-none'
-                            placeholder={t('form.diskCapacity')}
-                            required="true"
-                        />
-                    </label>
-
-                    <label className='input input-bordered flex items-center gap-2'>
-                        <input
-                            type="text"
-                            className='flex-1 border-none outline-none'
-                            placeholder={t('form.processor')}
-                            required="true"
-                        />
-                    </label>
-
-                    <button className='w-full rounded-md bg-Blue900 px-4 py-2 font-semibold text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'>
-                        {t('form.submit')}
-                    </button>
-                    <Link to="/deviceManagment">
-                        <div>
-                            <button className='w-full rounded-md bg-Blue900 px-4 py-2 font-semibold text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'>
-                                {t('back.text')}
-                            </button>
-                        </div>
-                    </Link>
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div className='flex min-h-screen items-center justify-center p-4'>
+      <div className='h-[700px] w-full max-w-md rounded-lg bg-Blue100 p-6 shadow-lg'>
+        <h2 className='mb-6 text-center text-2xl font-bold text-gray-800'>
+          {t('form.tittleform')}
+        </h2>
+        <form className='space-y-4'>
+          <label className='input input-bordered flex items-center gap-2'>
+            <select
+              className='flex-1 border-none bg-white outline-none'
+              placeholder={t('form.equipmentType')}
+            >
+              <option value=''>Usuarios</option>
+              <option value='desktop'>{t('form.desktop')}</option>
+              <option value='laptop'>{t('form.laptop')}</option>
+            </select>
+          </label>
+          <label className='input input-bordered flex items-center gap-2'>
+            <select
+              className='flex-1 border-none bg-white outline-none'
+              placeholder={t('form.equipmentType')}
+            >
+              <option value=''>Marca</option>
+              <option value='desktop'>{t('form.desktop')}</option>
+              <option value='laptop'>{t('form.laptop')}</option>
+            </select>
+          </label>
+          <label className='input input-bordered flex items-center gap-2'>
+            <input
+              type='text'
+              className='flex-1 border-none outline-none'
+              placeholder={t('form.serial')}
+              required='true'
+            />
+          </label>
+          <label className='input input-bordered flex items-center gap-2'>
+            <select
+              className='flex-1 border-none bg-white outline-none'
+              placeholder={t('form.equipmentType')}
+            >
+              <option value=''>{t('form.selectType')}</option>
+              <option value='desktop'>{t('form.desktop')}</option>
+              <option value='laptop'>{t('form.laptop')}</option>
+            </select>
+          </label>
+          <label className='input input-bordered flex items-center gap-2'>
+            <input
+              type='text'
+              className='flex-1 border-none outline-none'
+              placeholder={t('form.ramCapacity')}
+              required='true'
+            />
+          </label>
+          <label className='input input-bordered flex items-center gap-2'>
+            <input
+              type='text'
+              className='flex-1 border-none outline-none'
+              placeholder={t('form.diskCapacity')}
+              required='true'
+            />
+          </label>
+          <label className='input input-bordered flex items-center gap-2'>
+            <input
+              type='text'
+              className='flex-1 border-none outline-none'
+              placeholder={t('form.processor')}
+              required='true'
+            />
+          </label>
+          <div className='flex w-full flex-col items-center justify-center gap-6 text-center'>
+            <button className='w-full rounded-md bg-Blue900 px-4 py-2 font-semibold text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'>
+              {t('form.submit')}
+            </button>
+            <NavLink
+              to='/deviceManagment'
+              className='w-full rounded-md bg-Blue900 px-4 py-2 font-semibold text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
+            >
+              {t('back.text')}
+            </NavLink>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
-export default Device;
+export default Device
